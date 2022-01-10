a4c03ec79244b562e97f3165d1b1da359bd10524
Svetlana <svetlana.samrinova@yandex.ru>
2022-01-10 23:35:58 +0300

    v4

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -3,14 +3,14 @@ package ru.netology.web;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+//import org.junit.jupiter.api.Test;
+//import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.openqa.selenium.By.cssSelector;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.openqa.selenium.By.cssSelector;
 
 public class CallbackTest {
     private WebDriver driver;
@@ -35,15 +35,5 @@ public class CallbackTest {
         driver = null;
     }
 
-    @SuppressWarnings("RedundantThrows")
-    @Test
-    void shouldTestHappyPath() throws InterruptedException {
-        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Петр Иванов");
-        driver.findElement(cssSelector("[type='tel']")).sendKeys("+79119111111");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.tagName("button")).click();
-        String actualText = driver.findElement(cssSelector("[data-test-id=order-success]")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText.trim());
-    }
 }
 
